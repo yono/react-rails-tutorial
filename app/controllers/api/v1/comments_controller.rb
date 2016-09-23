70b5f6b0fc7b8088f880cc821e8be762458ab328
Yusaku ONO <yono.05@gmail.com>
2016-09-23 21:53:30 +0900

    add POST action

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -2,4 +2,15 @@ class Api::V1::CommentsController < ApplicationController
   def index
     @data = Comment.all
   end
+
+  def create
+    @comment = Comment.create(comment_params)
+    render :show, status: :created
+  end
+
+  private
+
+  def comment_params
+    params.permit(:author, :text)
+  end
 end
