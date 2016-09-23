d78e2a6fda89518b5b5089afc29184160774ddf5
Yusaku ONO <yono.05@gmail.com>
2016-09-23 21:50:35 +0900

    render data from DB

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,8 +1,5 @@
 class Api::V1::CommentsController < ApplicationController
   def index
-    @data = [
-      { author: 'Pete Hunt', text: 'This is one comment' },
-      { author: 'Jordan Walke', text: 'This is *another* comment' }
-    ]
+    @data = Comment.all
   end
 end
